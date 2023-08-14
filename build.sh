fc39644b4966965b9f21663200c619d2de049a0d
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2023-08-14 09:25:27 -0700

    Copy moonshell
    also, ignore out dir

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+# prep
+rm -rf out
+
+# make flashcart dirs
 mkdir -p out/acep
 mkdir -p out/r4it
 mkdir -p out/r4xx
@@ -19,3 +23,17 @@ cp kernel/wood_r4xx.dat out/r4xx/wood.dat
 cp -r aos_configs out/acep/__rpg/extention
 cp -r aos_configs out/r4it/__rpg/extention
 cp -r aos_configs out/r4xx/__rpg/extention
+
+# prep for app copy
+mkdir -p out/acep/__rpg/APP
+mkdir -p out/r4it/__rpg/APP
+mkdir -p out/r4xx/__rpg/APP
+
+# copy moonshl2
+cp -r moonshell_210/moonshl2 out/acep/moonshl2
+cp -r moonshell_210/moonshl2 out/r4it/moonshl2
+cp -r moonshell_210/moonshl2 out/r4xx/moonshl2
+# and the loader to where it needs to go
+cp moonshell_210/moonshl2.nds out/acep/__rpg/APP/moonshl2.nds
+cp moonshell_210/moonshl2.nds out/r4it/__rpg/APP/moonshl2.nds
+cp moonshell_210/moonshl2.nds out/r4xx/__rpg/APP/moonshl2.nds
